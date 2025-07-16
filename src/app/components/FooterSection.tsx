7893f5c9c71a0e4b1f936bcf48a578c63e29408f
Kanishk Jhala <kanishk.jhala@gradding.com>
2025-07-16 18:05:20 +0530

    Fix CSS for Button in footer

--- src/app/components/FooterSection.tsx
+++ src/app/components/FooterSection.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 text-white py-4">
+    <footer className="bg-gradient-to-r from-[#2e194d] to-[#baa6da] hover:from-[#baa6da] hover:to-[#2e194d] text-white py-4">
       <div className="text-center">
         <p className="text-sm sm:text-base font-medium">
           Developed and managed by{" "}
